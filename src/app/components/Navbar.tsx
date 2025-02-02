3b6f27d390dbadabb5e5c7968d7b89a95c8f1225
Arunraj <arunraj@neokred.tech>
2025-02-02 13:45:21 +0530

    updated favicon, success page addded, home route added from logo

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,22 +1,32 @@
 "use client";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
-  return (
-<nav className="fixed top-0 left-0 w-full flex items-center justify-end px-8 py-4 
-  bg-transparent backdrop-blur-md  md:backdrop-blur-none  z-50">
+  const route = useRouter();
 
-  <div className="flex items-center z-5 space-x-3">
-  <Image 
-          src="/mmLogo.png" 
-          alt="Make My Web App Logo" 
-          width={160} 
-          height={72} 
-          className="object-contain" 
+  const handleNavigate = () => {
+    route.push("/");
+  };
+
+  return (
+    <nav
+      className="fixed top-0 left-0 w-full flex items-center justify-end px-8 py-4 
+  bg-transparent backdrop-blur-md  md:backdrop-blur-none  z-50"
+    >
+      <div
+        onClick={handleNavigate}
+        className="flex cursor-pointer items-center z-5 space-x-3"
+      >
+        <Image
+          src="/mmLogo.png"
+          alt="Make My Web App Logo"
+          width={80}
+          height={32}
+          className="object-contain"
         />
-  </div>
-</nav>
-  
+      </div>
+    </nav>
   );
 }
